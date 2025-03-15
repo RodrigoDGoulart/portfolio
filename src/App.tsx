ebc20679a5ca48a0d897659f8e0c15c1c39305e0
RodrigoDGoulart <rodinizgoulart@gmail.com>
2025-03-15 12:09:52 -0300

    feat: title component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import styles from './App.module.scss';
 import Title from './components/Title';
+import styles from './App.module.scss'
 
 function App() {
   return (
     <>
-      <Title></Title>
-      <div className={styles.test}>teste</div>
+      <Title size='banner'>teste <b>teste</b></Title>
+      <Title size='h1'>teste <b>teste</b></Title>
+      <Title size='h2'>teste <b>teste</b></Title>
+      <Title size='h2' className={styles.test}>teste <b>teste</b></Title>
     </>
   )
 }
--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,5 +1,24 @@
-export default function Title() {
-  return(
-    <h1>TESTE</h1>
-  )
+import classNames from "classnames";
+import styles from './Title.module.scss';
+
+interface Props {
+  size?: "h1" | "h2" | "banner";
+  children: React.ReactNode;
+  hightlight?: boolean;
+  className?: string;
+}
+
+export default function Title({ size = "h1", children, hightlight, className }: Props) {
+  return (
+    <h1
+      className={classNames({
+        [styles.title]: true,
+        [styles[size]]: true,
+        [styles.highlight]: hightlight,
+        [className || ""]: true,
+      })}
+    >
+      {children}
+    </h1>
+  );
 }
