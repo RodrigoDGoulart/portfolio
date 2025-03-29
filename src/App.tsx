69ac7624d40a4855e3046b3b6d2bd311e4872490
RodrigoDGoulart <rodinizgoulart@gmail.com>
2025-03-29 17:05:23 -0300

    feat: text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import Title from './components/Title';
-import styles from './App.module.scss'
+import Text from './components/Text';
 
 function App() {
   return (
     <>
-      <Title size='banner'>teste <b>teste</b></Title>
-      <Title size='h1'>teste <b>teste</b></Title>
-      <Title size='h2'>teste <b>teste</b></Title>
-      <Title size='h2' className={styles.test}>teste <b>teste</b></Title>
+      <Text detach>teste <b>teste</b></Text>
     </>
   )
 }
---
+++ src/components/Text/index.tsx
@@ -0,0 +1,24 @@
+import classNames from "classnames";
+import styles from "./Text.module.scss";
+
+interface Props extends React.HTMLAttributes<HTMLParagraphElement> {
+  children: React.ReactNode;
+  detach?: boolean;
+}
+
+export default function Text({ children, ...props }: Props) {
+  return (
+    <p
+      className={classNames(
+        styles.text,
+        {
+          [styles.detach]: props.detach,
+        },
+        props.className
+      )}
+      {...props}
+    >
+      {children}
+    </p>
+  );
+}
